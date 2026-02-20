ec4c242279d13c4e890f18a8f4dc76952d0c3947
camily <mcamilaandrea229@gmail.com>
2026-02-19 19:05:56 -0500

    nuevo-switch

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,35 @@
-public class Main{
-    plubic static vold main(String[] args){
-        System.out.println("hola mundo");
-        System.out.println("repaso comando git");
-        System.out.println("camila andrea moreno");
-        System.out.println("pyton");
-    }
+import java.util.Scanner;
 
-}
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("""
+        1- lunes
+        2- martes
+        3- miercoles
+        4- jueves
+        5- viernes
+        6- sabado
+        7- domingo
+        """);
+
+        System.out.println("Ingrese el numero correspondiente al dia de la semana:");
+
+        int dia = teclado.nextInt();
+
+        switch (dia) {
+            case 1 -> System.out.println("Es lunes");
+            case 2 -> System.out.println("Es martes");
+            case 3 -> System.out.println("Es miercoles");
+            case 4 -> System.out.println("Es jueves");
+            case 5 -> System.out.println("Es viernes");
+            case 6 -> System.out.println("Es sabado");
+            case 7 -> System.out.println("Es domingo");
+            default -> System.out.println("Numero invalido");
+        }
+
+        teclado.close();
+    }
